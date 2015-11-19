023779712adfeb6109f9bfd86253fc86eaf3de5e
EntranceJew <EntranceJew@gmail.com>
2015-11-19 14:23:59 -0500

    Added code, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # poopsmith.lua
 The much less useful alternative to strict.lua
+
+Because having a bunch of globals is messy.
+Drop this in and any project that relies on globals will be rendered inoperable until it's fixed.
+
+# Usage
+```lua
+-- just require and invoke it in your preferred mode
+require('poopsmith')('janitor')
+
+-- then declare some globals
+a=420
+people_that_arent_jackie_chan = {
+	"the guy from cheers",
+	"Kelsey Grammer",
+	"Edgar Allan Poe"
+}
+ten_things_that_arent_jackie_chan = {
+	"toothpaste",
+	"pizza",
+	"lamps",
+	"jackie ch--"
+}
+
+-- show off the globals
+for k,v in pairs(_G["💩"]) do
+	print(k,v)
+end
+```
+
+# Modes
+## janitor
+Uses `_G["💩"]` as a container for all global variables with names preserved.
+```
+💩       								table: 0x2c56aea0
+💩.a       								420
+💩.people_that_arent_jackie_chan			table: 0x2c56af00
+💩.ten_things_that_arent_jackie_chan		table: 0x2c53ea58
+```
+## tamagotchi
+Uses `_G["💩"]` to determine how long the next variable name should be.
+```
+💩		4
+💩💩		420
+💩💩💩	table: 0x1ba0cba0
+💩💩💩💩	table: 0x1ba0cc18
+```
+## beanCounter
+Like Tamagotchi, uses `_G["💩"]` to change the variable name to the order it was declared, globally.
+```
+💩       3
+💩1      420
+💩2      table: 0x2437eb80
+💩3      table: 0x2437ec10
+```
+
+# Advanced Usage
+Here are some other uses of this:
+```lua
+local poopsmith = require('poopsmith')
+
+-- easy unicode poop access 
+print(poopsmith.unipoop) -- prints "💩"
+-- this is the same, but it uses a character escaped string
+print(poopsmith.safepoop) -- prints "💩"
+
+-- keep tabs on variables in janitor mode
+for k,v in pairs(_G[poopsmith.unipoop]) do
+	print(k,v)
+end
+
+-- figure out how many globals you were responsible for in tamagotchi mode
+print(_G[poopsmith.unipoop])
+
+-- make your own mode
+poopsmith.modes['nonsense'] = {
+	__smithSetup = function() 
+		print('buckle up')
+	end,
+	__newindex = print
+}
+poopsmith.setMode('nonsense')
+a=3 -- prints "_G, 'a', 3"
+```
+
+# Why?
+Because LuaJIT lets me use unicode variable references, this is the natural extension of that.
+Despite that, this will work with non-LuaJIT environments.
+
+Also because it's fun.
---
+++ poopsmith.lua
@@ -0,0 +1,77 @@
+local poopsmith = {
+	_VERSION     = 'poopsmith 1.0.0',
+	_DESCRIPTION = 'The much less useful alternative to strict.lua',
+	_URL         = 'https://github.com/EntranceJew/poopsmith.lua',
+	_LICENSE     = [[
+		The MIT License (MIT)
+
+		Copyright (c) 2015 EntranceJew
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	]]
+}
+
+-- to share, with the lazy
+poopsmith.unipoop = "💩"
+poopsmith.safepoop = "\240\159\146\169"
+
+-- all the valid modes
+poopsmith.modes = {
+	tamagotchi = {
+		__smithSetup = function()
+			_G[poopsmith.unipoop] = 1
+		end,
+		__newindex = function(g, name, value)
+			_G[poopsmith.unipoop] = _G[poopsmith.unipoop] + 1
+			return rawset(_G, poopsmith.unipoop:rep(_G[poopsmith.unipoop]), value)
+		end
+	},
+	beanCounter = {
+		__smithSetup = function()
+			_G[poopsmith.unipoop] = 0
+		end,
+		__newindex = function(g, name, value)
+			_G[poopsmith.unipoop] = _G[poopsmith.unipoop] + 1
+			return rawset(_G, poopsmith.unipoop .. _G[poopsmith.unipoop], value)
+		end
+	},
+	janitor = {
+		__smithSetup = function()
+			_G[poopsmith.unipoop] = {}
+		end,
+		__newindex = function(g, name, value)
+			return rawset(_G[poopsmith.unipoop], name, value) 
+		end
+	}
+}
+
+function poopsmith.setMode(mode)
+	if not poopsmith.modes[mode] then
+		return false
+	end
+	
+	if poopsmith.modes[mode].__smithSetup then
+		poopsmith.modes[mode].__smithSetup()
+	end
+	
+	setmetatable(_G, poopsmith.modes[mode])
+	return true
+end
+
+return setmetatable(poopsmith, {__call = function(_, mode) poopsmith.setMode(mode) end})
